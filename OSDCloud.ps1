0ff27cfd43a829a5fa8f504fea651fc0e0107657
Disrupi <95573431+Disrupi@users.noreply.github.com>
2024-06-01 01:42:05 +0200

    Update OSDCloud.ps1

--- OSDCloud.ps1
+++ OSDCloud.ps1
@@ -6,5 +6,6 @@ Install-Module OSD -Force -Skippublishercheck
 #Start-OSDCloud -UpdateFirmware -OSName 'Windows 11 22H2 x64' -OSLanguage en-us -OSEdition Enterprise -zti
 #Start-OSDCloud -OSName 'Windows 11 22H2 x64' -OSLanguage en-us -OSEdition Enterprise -zti
 Start-OSDCloud -OSVersion 'Windows 11' -OSLanguage en-us -OSBuild 22H2 -OSEdition Enterprise -ZTI 
+start-sleep -seconds 5
 
 wpeutil reboot
