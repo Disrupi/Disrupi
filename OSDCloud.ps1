20059617b5d85a39c8deff925d465a1cf11d30ac
Disrupi <95573431+Disrupi@users.noreply.github.com>
2024-06-01 01:19:04 +0200

    Update OSDCloud.ps1

--- OSDCloud.ps1
+++ OSDCloud.ps1
@@ -3,6 +3,6 @@ Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
 
 Install-Module OSD -Force -Skippublishercheck
 
-Start-OSDCloud -Firmware -WindowsUpdate -OSName 'Windows 11 22H2 x64' -OSLanguage en-us -OSEdition Enterprise -zti
+Start-OSDCloud -Firmware -OSName 'Windows 11 22H2 x64' -OSLanguage en-us -OSEdition Enterprise -zti
 
 wpeutil reboot
