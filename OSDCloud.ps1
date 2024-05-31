283d8f8fc296e4ec6d170da25b13daad69956a06
Disrupi <95573431+Disrupi@users.noreply.github.com>
2024-06-01 01:21:32 +0200

    Update OSDCloud.ps1

--- OSDCloud.ps1
+++ OSDCloud.ps1
@@ -6,4 +6,4 @@ Install-Module OSD -Force -Skippublishercheck
 $OSDModuleResource.OSDCloud.Default
 #Start-OSDCloud -Firmware -OSName 'Windows 11 22H2 x64' -OSLanguage en-us -OSEdition Enterprise -zti
 
-wpeutil reboot
+#wpeutil reboot
