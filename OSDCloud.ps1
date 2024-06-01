3fac937e8ca904a8f49a74bffb769338662b4012
Disrupi <95573431+Disrupi@users.noreply.github.com>
2024-06-01 15:21:05 +0200

    Update OSDCloud.ps1

--- OSDCloud.ps1
+++ OSDCloud.ps1
@@ -4,12 +4,14 @@ Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
 Install-Module OSD -Force -Skippublishercheck
 
 $Params = @{
-    OSVersion = "Windows 10"
+    OSVersion = "Windows 11"
     OSBuild = "22H2"
     OSEdition = "Pro"
     OSLanguage = "en-US"
     ZTI = $true
     Firmware = $true
+    DriverPack = $false
+    WindowsUpdateDrivers = $true
 }
 Start-OSDCloud @Params
 
