6b7d40b181973287f35ab820d93c3500a5638ad8
Disrupi <95573431+Disrupi@users.noreply.github.com>
2024-06-05 23:38:25 +0200

    Update OSDCloud.ps1

--- OSDCloud.ps1
+++ OSDCloud.ps1
@@ -22,7 +22,7 @@ $Params = @{
     OSVersion = "Windows 11"
     OSBuild = "22H2"
     OSEdition = "Enterprise"
-    #OSLanguage = "en-us"
+    OSLanguage = "de-de"
     ZTI = $true
     Firmware = $true
 }
